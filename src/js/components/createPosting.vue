59a390f0f27a9b4adc51207578d20963d0253d13
Adam Alexander <adam@botchthecrab.com>
2021-10-29 21:20:52 -0700

    create posting page

--- src/js/components/createPosting.vue
+++ src/js/components/createPosting.vue
@@ -31,7 +31,7 @@
 
       & > label {
         display: inline-block;
-        width: 110px;
+        width: 120px;
         text-align: right;
         margin-right: 10px;
       }
@@ -95,7 +95,7 @@
       <textarea id="create-posting-content" v-mode="posting.content" rows="20"></textarea>
     </div>
 
-    <div class="post-footer">
+    <div class="post-footer" v-bind:style="{ visibility: (editorLoaded ? 'visible' : 'hidden') }">
 
       <div class="post-footer-item">
         <label>Tags:</label>
@@ -126,7 +126,6 @@
 </template>
 
 <script>
-  console.info('createPosting.vue');
 
   // GLOBAL COMPONENTS
   var globalService = require('services/global_service');
@@ -164,10 +163,8 @@
     },
 
     beforeMount() {
-      console.info('beforeMount');
       vm = this;
-      this.initContentEditor();
-      this.initSelectors();
+      vm.initAuthorization();
     },
 
     mounted() {
@@ -176,44 +173,41 @@
 
     methods: {
 
+      initAuthorization: function() {
+
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            console.log('SIGNED IN!');
+            vm.initContentEditor();
+            vm.initSelectors();
+
+          } else {
+            console.log('NOT SIGNED IN!');
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+          }
+        });
+
+      },
+
       initContentEditor: function() {
         var s = document.createElement('script');
         var tinymceApiKey = "casdhqgylieh1y3j27nsdxsrpn4f1qtupm3zddbpqzvref26";
         s.src = "https://cdn.tiny.cloud/1/" + tinymceApiKey + "/tinymce/5/tinymce.min.js";
         s.type = "text/javascript";
         s.referrerPolicy = "origin";
-        // s.async = false;
         s.onload = function() {
-          console.info('tinymce script was loaded');
-
           vm.tinymce = tinymce;
-
-          console.info({
-            tinymce: tinymce
-          });
 
           var tinymceOptions = {
             selector: '#create-posting-content',
-            // base_url: '/deploy',
-
-            // content_css : "/css/cassette.css"
-
             plugins: 'image code lists charmap fullscreen media link',
-            // toolbar: 'code',
-            // menubar: 'tools',
-
             toolbar1: 'bold italic underline strikethrough | aligncenter | outdent indent | numlist bullist',
             toolbar2: 'charmap | fullscreen | image media link | code',
-
-
             skin: 'oxide-dark',
             content_css: 'dark, /css/cassette.css',
             content_style: 'body { text-align: left; margin: 10px; }'
-
           };
-          console.info({
-            tinymceOptions: tinymceOptions
-          });
 
           tinymce.init(tinymceOptions);
 
@@ -231,23 +225,14 @@
         var s = document.createElement('script');
         s.src = "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js";
         s.type = "text/javascript";
-        s.onload = function() {
-          console.info('select2 script loaded');
-        };
         $head.appendChild(s);
 
         var allCategoriesRequest = vm.getAllCategories().then(function(response) {
-          console.info({
-            allCategories: response
-          });
           var categoryData = _.map(response, function(category) {
             return {
               id: category.categoryId,
               text: category.name
             };
-          });
-          console.info({
-            categoryData: categoryData
           });
 
           $('#create-posting-categories').select2({
@@ -258,10 +243,6 @@
         });
 
         var allTagsRequest = vm.getAllTags().then(function(response) {
-          console.info({
-            allTags: response
-          });
-
           var tagData = _.map(response, function(tag) {
             return {
               id: tag.tagId,
@@ -269,9 +250,6 @@
             };
           });
           tagData = _.sortBy(tagData, 'text');
-          console.info({
-            tagData: tagData
-          });
 
           $('#create-posting-tags').select2({
             data: tagData,
@@ -295,8 +273,6 @@
       },
 
       getContent: function() {
-        console.info('getContent ...');
-
         vm.posting.content = vm.tinymce.activeEditor.getContent();
 
         var postTime = $('#create-posting-posted').val();
@@ -314,46 +290,54 @@
           return Number(selection.id);
         });
 
-        console.info({
-          "vm.posting": vm.posting
-        });
-
         return vm.posting;
       },
 
       createPosting: function() {
         var posting = vm.getContent();
 
-        // if (!posting.title) {
-        //   window.alert("Title is missing.");
-        //   return;
-        // }
-        // if (!posting.content) {
-        //   window.alert("Content is missing.");
-        //   return;
-        // }
-        // if (!posting.posted) {
-        //   window.alert("Posted time is missing.");
-        //   return;
-        // }
+        if (!posting.title) {
+          window.alert("Title is missing.");
+          return;
+        }
+        if (!posting.content) {
+          window.alert("Content is missing.");
+          return;
+        }
+        if (!posting.tagIds.length) {
+          window.alert("Tags are missing.");
+          return;
+        }
+        if (!posting.categoryIds.length) {
+          window.alert("Categories are missing.");
+          return;
+        }
+        if (!posting.posted) {
+          window.alert("Posted time is missing.");
+          return;
+        }
 
         blogService.getAllPostings().then(function(response) {
           var allPostings = response.val();
-          console.info({
-            allPostings: allPostings
-          });
-
           var nextEntryIndex = allPostings.length;
           var nextPostingId = _.max(allPostings, 'postingId').postingId + 1;
-          console.info({
-            nextEntryIndex: nextEntryIndex,
-            nextPostingId: nextPostingId
-          });
-
           posting.postingId = nextPostingId;
 
-          console.info('JSON to save and import ...')
-          console.info(JSON.parse(JSON.stringify(posting)));
+          var postingUpdate = {};
+          postingUpdate['blog/postings/' + (nextPostingId - 1)] = posting;
+
+          return firebase.database().ref().update(postingUpdate).then(function(response) {
+            window.alert("Your posting was successfully submitted!");
+
+            vm.$router.push({
+              path: '/'
+            });
+
+          }, function(error) {
+            console.error(error);
+            window.alert("There was an error attempting to submit your posting.");
+          });
+
         });
 
       }
